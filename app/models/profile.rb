cc093e8b5dad869ee98d8f48c81460adc5ed353d
AliakseiLeanovich <aliaksei.leanovich@gmail.com>
2016-05-24 05:40:12 +0300

    Add validations to profiles

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,15 @@
 class Profile < ActiveRecord::Base
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :name, length: { minimum: 5 }
+  validates :description, presence: true
+  validates :lng, presence: true
+  validates :lat, presence: true
+  validates :rating, presence: true
+  validates :rating, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5
+  }
   belongs_to :user
 end
